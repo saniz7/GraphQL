0c06d8681605b49d201c7bff4ca3621e43fdf068
Sanij <Sanizsanij@gmail.com>
2023-12-01 11:17:26 +0545

    feat[clientside]:setup apolloclient

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloProvider, useApolloClient, InMemoryCache, ApolloClient } from '@apollo/client';
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
 
+});
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
